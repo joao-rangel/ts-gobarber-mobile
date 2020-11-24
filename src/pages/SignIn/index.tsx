8711164219ca32cda857a03a01e6fee52813cbde
Joao Rangel <joao.vran@gmail.com>
2020-11-24 00:02:41 -0300

    started pages files to add routes

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Text>Olá mundo!</Text>
+  </Container>
+);
+
+export default SignIn;

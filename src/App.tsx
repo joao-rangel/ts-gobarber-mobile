ab7c93288ea8a9c0c7c1f6058a829f54d27a16e1
Joao Rangel <joao.vran@gmail.com>
2020-11-23 23:27:27 -0300

    eslint finally working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 import { View, StatusBar } from 'react-native';
 
 const App: React.FC = () => (
   <>
-    <StatusBar barStyle='light-content' backgroundColor="#312e38" />
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
     <View style={{ flex: 1, backgroundColor: '#312e38' }} />
   </>
 );

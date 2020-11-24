f0fdad88ad9a1a6f8b67b7f9dfc8ccfdf5478658
Joao Rangel <joao.vran@gmail.com>
2020-11-24 20:40:47 -0300

    add input and button to SignIn

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
 
     <Title>Faça seu login</Title>
+
+    <Input name="email" icon="mail" placeholder="E-mail" />
+
+    <Input name="password" icon="lock" placeholder="Senha" />
+
+    <Button
+      onPress={() => {
+        console.log('Entrando...'); // TODO
+      }}
+    >
+      Entrar
+    </Button>
   </Container>
 );
 

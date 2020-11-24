00f2da6c5b9137133a5c3cc45980a6c14e91581e
Joao Rangel <joao.vran@gmail.com>
2020-11-23 21:43:04 -0300

    started App

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { View, StatusBar } from 'react-native';
+
+const App: React.FC = () => <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+
+export default App;

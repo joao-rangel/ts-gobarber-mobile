797b9340cfa31877894f3871169640c4bb15b9cc
Joao Rangel <joao.vran@gmail.com>
2020-11-25 22:20:33 -0300

    input color style on focus or filled

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,12 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-export const Container = styled.View`
+interface ContainerProps {
+  isFocused: boolean;
+}
+
+export const Container = styled.View<ContainerProps>`
   width: 100%;
   height: 60px;
   padding: 0 16px;
@@ -12,6 +16,15 @@ export const Container = styled.View`
 
   flex-direction: row;
   align-items: center;
+
+  border-width: 2px;
+  border-color: #232129;
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+    `}
 `;
 
 export const TextInput = styled.TextInput`

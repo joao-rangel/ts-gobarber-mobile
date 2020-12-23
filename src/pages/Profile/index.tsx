908a46476b8bf0c83e75155eba14a288695fb1a7
Joao Rangel <joao.vran@gmail.com>
2020-12-23 18:17:28 -0300

    add imagePicker to change profile image

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -11,6 +11,7 @@ import { useNavigation } from '@react-navigation/native';
 import * as Yup from 'yup';
 import Icon from 'react-native-vector-icons/Feather';
 
+import ImagePicker from 'react-native-image-picker/src';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import { useAuth } from '../../hooks/auth';
@@ -125,6 +126,36 @@ const Profile: React.FC = () => {
     [navigation, updateUser],
   );
 
+  const handleUpdateAvatar = useCallback(() => {
+    ImagePicker.showImagePicker(
+      {
+        title: 'Selecione um avatar',
+        cancelButtonTitle: 'Cancelar',
+        takePhotoButtonTitle: 'Usar câmera',
+        chooseFromLibraryButtonTitle: 'Escolher da galeria',
+      },
+      response => {
+        if (response.didCancel) return;
+        if (response.error) {
+          Alert.alert('Erro ao atualizar seu avatar.');
+          return;
+        }
+
+        const data = new FormData();
+
+        data.append('avatar', {
+          type: 'image/jpeg',
+          name: `${user.id}.jpg`,
+          uri: response.uri,
+        });
+
+        api.patch('users/avatar', data).then(apiResponse => {
+          updateUser(apiResponse.data);
+        });
+      },
+    );
+  }, [updateUser, user.id]);
+
   const navigateBack = useCallback(() => {
     goBack();
   }, [goBack]);
@@ -146,7 +177,7 @@ const Profile: React.FC = () => {
                 <Icon name="chevron-left" size={24} color="#999591" />
               </BackButton>
 
-              <UserAvatarButton onPress={() => {}}>
+              <UserAvatarButton onPress={handleUpdateAvatar}>
                 {/* TODO implement handleAvatarChange */}
                 <UserAvatar source={{ uri: user.avatar_url }} />
               </UserAvatarButton>
